0fe2c1c3017cb0b7cae93cec47ce48292e673cd1
Alexis Cuellar <alexisalra@hotmail.com>
2019-05-21 20:00:13 -0600

    Creación de las rutas de la aplicación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { AlimentosComponent } from './alimentos/alimentos.component';
+import { BebidasComponent } from './bebidas/bebidas.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes =
+[
+  { path: 'principal', component: PrincipalComponent},
+  { path: 'alimentos', component: AlimentosComponent},
+  { path: 'bebidas', component: BebidasComponent},
+  { path: '**', component: PrincipalComponent}
+]
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppModule { }
